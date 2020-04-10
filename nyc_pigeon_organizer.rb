c57f8a009d4c65ec472c05837715f5e3fe0cff49
Issa Sangare <issabmsangare@hotmail.com>
2020-04-10 13:50:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  nyc_pigeon_organiser = {}
+  data.each do |attribute, pigeon_data| 
+   pigeon_data.each do |attribute_value, pigeons| 
+       pigeons.each do |pigeons_names|
+         
+           nyc_pigeon_organiser[pigeons_names] = [ attribute] 
+         binding.pry
+          
+        end
+          #nyc_pigeon_organiser [pigeons_names][attribute] = [attribute_value]
+          
+       end 
+        
+      
+     end
+     nyc_pigeon_organiser
+  end
+  
