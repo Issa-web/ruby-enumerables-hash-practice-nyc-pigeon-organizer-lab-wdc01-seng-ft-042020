a07e19bba014140f6cbea56f1479e2c0595eb126
Issa Sangare <issabmsangare@hotmail.com>
2020-04-10 16:39:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  nyc_pigeon_organiser = {}
+  data.each do |attribute, pigeon_data| 
+   pigeon_data.each do |attribute_value, pigeons| 
+       pigeons.each do |pigeons_names|
+         
+          if nyc_pigeon_organiser[pigeons_names] == nil 
+            nyc_pigeon_organiser[pigeons_names] = {}
+          end
+          
+          if nyc_pigeon_organiser[pigeons_names][attribute] == nil
+         nyc_pigeon_organiser[pigeons_names][attribute] = [ ]
+       end 
+         nyc_pigeon_organiser[pigeons_names][attribute].push(attribute_value.to_s )
+           
+        end
+         
+       end 
+        
+     end
+     #binding.pry
+     nyc_pigeon_organiser
+  end
+  
