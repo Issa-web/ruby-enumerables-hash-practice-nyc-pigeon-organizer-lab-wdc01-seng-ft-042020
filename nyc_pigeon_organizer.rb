6cb0644e660b5ffbe9c56984b2384086dfe0ed06
Issa Sangare <issabmsangare@hotmail.com>
2020-04-10 14:58:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
          
            nyc_pigeon_organiser[pigeons_names] = [ attribute]
           
-         nyc_pigeon_organiser [pigeons_names] [attribute] = [attribute_value]
+         nyc_pigeon_organiser [pigeons_names][:attribute] = [attribute_value]
           #binding.pry 
         end
          
